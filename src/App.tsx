a1578f8b8a8ea42c22f067c089c16f2d5cd82458
razaaliwebdev <razaali.webdev@gmail.com>
2025-10-11 22:39:28 +0500

    initial routing  like auth and home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import ResetPassword from "./pages/auth/ResetPassword";
+import VerifyOtp from "./pages/auth/VerifyOtp";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import SignUp from "./pages/auth/SignUp";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Toaster position="top-center" />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/signup" element={<SignUp />} />
+        <Route path="/auth/forgot-password" element={<ForgotPassword />} />
+        <Route path="/auth/verify-otp" element={<VerifyOtp />} />
+        <Route path="/auth/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/redux/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: {
+        // Reducers will be added here
+    }
+})
+
+export default store;
