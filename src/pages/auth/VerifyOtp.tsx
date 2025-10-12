33c82c8fd05cca1e6cb2d1dbc93d47052a534257
razaaliwebdev <razaali.webdev@gmail.com>
2025-10-13 01:33:04 +0500

    remove import react

--- src/pages/auth/VerifyOtp.tsx
+++ src/pages/auth/VerifyOtp.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const VerifyOtp = () => {
-  return (
-    <div>VerifyOtp</div>
-  )
-}
+  return <div>VerifyOtp</div>;
+};
 
-export default VerifyOtp
+export default VerifyOtp;
