ac215dc7ce64d19d41de4864fff9000d2561e7f1
razaaliwebdev <razaali.webdev@gmail.com>
2025-10-13 13:14:52 +0500

    fix something in the daile special

--- src/components/common/Landing/DailySpecial.tsx
+++ src/components/common/Landing/DailySpecial.tsx
@@ -3,16 +3,16 @@ import { dailySpecial } from "@/assets/assets";
 const DailySpecial = () => {
   return (
     <section className="md:my-10">
-      <h2 className="md:text-xl font-bold text-lg">Daily Special</h2>
+      <h2 className="md:text-xl font-bold text-lg mt-5">Daily Special</h2>
       <div className="flex justify-center">
         <div className="grid md:grid-cols-4 grid-cols-1 md:gap-8 my-2">
           {dailySpecial.map((item) => {
             return (
               <div
                 key={item.id}
-                className="flex items-center justify-center flex-col px-8 border-[1px] border-gray-300 rounded   my-5 hover:border-orange-400 transition-all "
+                className="flex items-center justify-center flex-col md:px-8 py-5 px-5 border-[1px] border-gray-200 rounded   my-5 hover:border-orange-400 hover:shadow hover:shadow-orange-400 transition-all "
               >
-                <div className="img w-74">
+                <div className="img md:w-74 w-55">
                   <img
                     src={item.image}
                     className="h-full w-full object-cover"
